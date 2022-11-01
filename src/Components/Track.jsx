3e7e7b22600adaa9dec76b078f6c1337b7a77bcf
Youssef Galal <youssefgalalnazem@gmail.com>
2022-11-01 19:00:33 +0200

    Get track audio when track is clicked

--- src/Components/Track.jsx
+++ src/Components/Track.jsx
@@ -1,6 +1,15 @@
+import axios from "axios";
+
 const Track = ({ track }) => {
+  const getAudio = () => {
+    axios
+      .get(`https://wmlhgm.deta.dev/audio/${track.watch_id}`)
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <div className="track" onMouseUp={(e)=>console.log(e.target)}>
+    <div className="track" onMouseUp={(e) => getAudio()}>
       <img src={track.thumbnail_url} alt={`${track.watch_id}`} />
       <div className="track-details">
         <div className="track-title">{track.title}</div>
