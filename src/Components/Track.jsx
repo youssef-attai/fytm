646b3c2b47100b27f4f70ba410487546959a061a
Youssef Galal <youssefgalalnazem@gmail.com>
2022-11-01 18:55:18 +0200

    Make tracks clickable

--- src/Components/Track.jsx
+++ src/Components/Track.jsx
@@ -1,6 +1,6 @@
 const Track = ({ track }) => {
   return (
-    <div className="track">
+    <div className="track" onMouseUp={(e)=>console.log(e.target)}>
       <img src={track.thumbnail_url} alt={`${track.watch_id}`} />
       <div className="track-details">
         <div className="track-title">{track.title}</div>
