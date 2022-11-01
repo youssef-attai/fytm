d7ce3c66cdf9a85a724f9cc7845a329f9ccc8b0f
Youssef Galal <youssefgalalnazem@gmail.com>
2022-11-01 18:52:33 +0200

    Add sidebar nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import Player from "./Player";
 import Home from "./Pages/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 const App = (props) => {
   return (
     <div className="app">
       <div className="container">
-        <div className="sidebar">
-          
-        </div>
         <Router>
+          <nav className="sidebar">
+            <Link className="nav-link" to="/">Home</Link>
+            <Link className="nav-link" to="/my">My playlist</Link>
+          </nav>
           <Routes>
             <Route path="/" element={<Home />} />
           </Routes>
+          <Player />
         </Router>
       </div>
-      <Player />
     </div>
   );
 };
