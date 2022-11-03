e39d8cefc40f3fd159ccb3b4b645cca98844c86e
Youssef Galal <youssefgalalnazem@gmail.com>
2022-11-03 13:56:58 +0200

    player slider

--- src/features/player/Player.jsx
+++ src/features/player/Player.jsx
@@ -8,7 +8,7 @@ import {
 } from "../../icons";
 import { nextInQueue, prevInQueue } from "../queue/queueSlice";
 import { play, pause } from "../player/playerSlice";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export function Player(props) {
   const tracks = useSelector((state) => state.queue.tracks);
@@ -16,6 +16,8 @@ export function Player(props) {
   const playing = useSelector((state) => state.player.playing);
   const audioRef = useRef();
 
+  const [sliderPosition, setSliderPosition] = useState(0);
+  const [sliderDown, setSliderDown] = useState(false);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -23,15 +25,32 @@ export function Player(props) {
       currentIndex >= 0 && currentIndex < tracks.length
         ? tracks[currentIndex].audio_url
         : "";
+  }, [tracks, currentIndex]);
+
+  useEffect(() => {
     if (playing) audioRef.current.play();
     else audioRef.current.pause();
-  }, [tracks, currentIndex, playing]);
+  }, [playing]);
+
+  // useEffect(() => {
+  //   if (audioRef.current.duration)
+  //     audioRef.current.currentTime =
+  //       sliderPosition * (audioRef.current.duration / 100);
+  //   else audioRef.current.currentTime = 0;
+  // }, [sliderPosition]);
 
   return (
     <div className="player">
       <audio
         ref={audioRef}
         src={""}
+        onTimeUpdate={(e) => {
+          if (!sliderDown)
+            setSliderPosition((e.target.currentTime / e.target.duration) * 100);
+        }}
+        onCanPlay={(e) => {
+          playing && e.target.play();
+        }}
         onEnded={(e) => {
           dispatch(nextInQueue());
         }}
@@ -63,7 +82,31 @@ export function Player(props) {
           }}
         />
       </div>
-      <input type="range" />
+      <input
+        className="player-slider"
+        type="range"
+        value={sliderPosition}
+        min={0}
+        max={100}
+        onInput={(e) => {
+          setSliderPosition(e.target.value);
+          if (audioRef.current.duration)
+            audioRef.current.currentTime =
+              sliderPosition * (audioRef.current.duration / 100);
+          else audioRef.current.currentTime = 0;
+        }}
+        onMouseDown={(e) => {
+          setSliderDown(true);
+        }}
+        onMouseUp={(e) => {
+          setSliderDown(false);
+          setSliderPosition(e.target.value);
+          if (audioRef.current.duration)
+            audioRef.current.currentTime =
+              sliderPosition * (audioRef.current.duration / 100);
+          else audioRef.current.currentTime = 0;
+        }}
+      />
     </div>
   );
 }
